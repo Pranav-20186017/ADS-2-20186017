e4342e44d0190eb0d0d6bfe87e536ae4394f6b7a
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-10-29 21:44:29 +0530

    "ads2_m1_a1_1: 7/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ads2-assignments/m1/1.1 Graph/Bag.java
+++ ads2-assignments/m1/1.1 Graph/Bag.java
@@ -1,26 +1,35 @@
+/*************************************************************************
+ *  Compilation:  javac Bag.java
+ *  Execution:    java Bag < input.txt
+ *
+ *  A generic bag or multiset, implemented using a linked list.
+ *
+ *************************************************************************/
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+
 /**
- * Class for bag.
- *
- * @param      <Item>  The item
+ *  The <tt>Bag</tt> class represents a bag (or multiset) of 
+ *  generic items. It supports insertion and iterating over the 
+ *  items in arbitrary order.
+ *  <p>
+ *  The <em>add</em>, <em>isEmpty</em>, and <em>size</em>  operation 
+ *  take constant time. Iteration takes time proportional to the number of items.
+ *  <p>
+ *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
+ *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
  */
 public class Bag<Item> implements Iterable<Item> {
-    /**
-     * number of elements in bag.
-     */
-    private int N;
-    /**
-     * beginning of bag.
-     */// 
-    private Node first;
-    /**
-     * Class for node.
-     */
+    private int N;         // number of elements in bag
+    private Node first;    // beginning of bag
+
+    // helper linked list class
     private class Node {
         private Item item;
         private Node next;
     }
+
    /**
      * Create an empty stack.
      */
@@ -28,8 +37,9 @@ public class Bag<Item> implements Iterable<Item> {
         first = null;
         N = 0;
     }
+
    /**
-     * Is the BAG empty?.
+     * Is the BAG empty?
      */
     public boolean isEmpty() {
         return first == null;
@@ -41,6 +51,7 @@ public class Bag<Item> implements Iterable<Item> {
     public int size() {
         return N;
     }
+
    /**
      * Add the item to the bag.
      */
@@ -51,17 +62,19 @@ public class Bag<Item> implements Iterable<Item> {
         first.next = oldfirst;
         N++;
     }
+
+
    /**
      * Return an iterator that iterates over the items in the bag.
      */
     public Iterator<Item> iterator()  {
         return new ListIterator();  
     }
-    /**
-     * Class for list iterator.
-     */
+
+    // an iterator, doesn't implement remove() since it's optional
     private class ListIterator implements Iterator<Item> {
         private Node current = first;
+
         public boolean hasNext()  { return current != null;                     }
         public void remove()      { throw new UnsupportedOperationException();  }
 
@@ -72,4 +85,5 @@ public class Bag<Item> implements Iterable<Item> {
             return item;
         }
     }
+
 }
