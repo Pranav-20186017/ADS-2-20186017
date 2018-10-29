7c537c73ecd6c54369eb9ae5f70bab95c4789f90
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-10-29 14:02:36 +0530

    "ads2_m1_a1_1: 0/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ads2-assignments/m1/1.1 Graph/Solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+interface Graph {
+    public int V();
+    public int E();
+    public void addEdge(int v, int w);
+    public Iterable<Integer> adj(int v);
+    public boolean hasEdge(int v, int w);
+}
+/**
+ * Class for solution.
+ */
+class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+		//unused.
+	}
+	/**
+	 * main function.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
+		Scanner sc = new Scanner(System.in);
+	}
+}
