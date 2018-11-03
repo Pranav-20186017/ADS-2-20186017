51b50cf438d39825d878e9fc27e028958954a1f1
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-11-03 11:12:30 +0530

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ads2-assignments/m6/ADS-2Week-1/Solution.java
+++ ads2-assignments/m6/ADS-2Week-1/Solution.java
@@ -1,6 +1,32 @@
-import java.util.Scanner;
+import java.util.Arrays;
 class PageRank {
-	
+    private Digraph pggraph;
+    private double[] prval;
+    PageRank(Digraph graph) {
+        this.pggraph = graph;
+        prval = new double[pggraph.V()];
+        for(int y = 0; y < prval.length; y++) {
+            prval[y] = (1.0 / (pggraph.V()));
+        }
+        updatingprvals();
+    }
+    void updatingprvals() {
+        for (int i = 0; i < 1000; i++) {
+            for (int j = 0; j < pggraph.V(); j++) {
+                double testprval = 0.0;
+                for (Integer eachadj : pggraph.adj(j)) {
+                    testprval += (getPR(eachadj)/pggraph.outdegree(eachadj));
+                }
+                prval[j] = testprval; 
+            }
+        }
+    }
+    double getPR(int v) {
+        return prval[v];
+    }
+    public String toString() {
+        return Arrays.toString(prval);
+    }
 }
 
 class WebSearch {
@@ -9,32 +35,39 @@ class WebSearch {
 
 
 public class Solution {
-	public static void main(String[] args) {
-		// read the first line of the input to get the number of vertices
-		Scanner sc = new Scanner(System.in);
-		int v = sc.nextInt();
-		System.out.println(v);
-		// iterate count of vertices times 
-		// to read the adjacency list from std input
-		// and build the graph
-		
-		
-		// Create page rank object and pass the graph object to the constructor
-		
-		// print the page rank object
-		
-		// This part is only for the final test case
-		
-		// File path to the web content
-		String file = "WebContent.txt";
-		
-		// instantiate web search object
-		// and pass the page rank object and the file path to the constructor
-		
-		// read the search queries from std in
-		// remove the q= prefix and extract the search word
-		// pass the word to iAmFeelingLucky method of web search
-		// print the return value of iAmFeelingLucky
-		
-	}
+    public static void main(String[] args) {
+        // read the first line of the input to get the number of vertices
+
+        int vertexnum = Integer.parseInt(StdIn.readLine());
+        // iterate count of vertices times
+
+        // to read the adjacency list from std input
+        // and build the graph
+        Digraph graph = new Digraph(vertexnum);
+        for (int i = 0; i < vertexnum; i++) {
+            String[] edges = StdIn.readLine().split(" ");
+            for (int k = 1; k < edges.length; k++) {
+                graph.addEdge(Integer.parseInt(edges[0]), Integer.parseInt(edges[k]));
+            }
+        }
+        System.out.println(graph);
+        // Create page rank object and pass the graph object to the constructor
+
+        PageRank pgrankobj = new PageRank(graph);
+        // print the page rank object
+        System.out.println(pgrankobj);
+        // This part is only for the final test case
+
+        // File path to the web content
+        String file = "WebContent.txt";
+
+        // instantiate web search object
+        // and pass the page rank object and the file path to the constructor
+
+        // read the search queries from std in
+        // remove the q= prefix and extract the search word
+        // pass the word to iAmFeelingLucky method of web search
+        // print the return value of iAmFeelingLucky
+
+    }
 }
