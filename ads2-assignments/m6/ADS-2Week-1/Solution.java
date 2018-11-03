e1f7c4c6b6419b78ffc6838838e8197dd6477350
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-11-03 12:18:47 +0530

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ads2-assignments/m6/ADS-2Week-1/Solution.java
+++ ads2-assignments/m6/ADS-2Week-1/Solution.java
@@ -1,59 +1,15 @@
 import java.util.Arrays;
-class PageRank {
-	private Digraph pggraph;
-	private double[] prval;
-	private double[] nowval;
-	PageRank(Digraph graph) {
-		this.pggraph = graph;
-		prval = new double[pggraph.V()];
-		for(int y = 0; y < prval.length; y++) {
-			prval[y] = (1.0 / (pggraph.V()));
-		}
-		nowval = new double[pggraph.V()];
-		updatingprvals();
-	}
-	void updatingprvals() {
-		for (int i = 0; i < 1000; i++) {
-			System.out.println("iteration number " + i);
-			for (int j = 0; j < pggraph.V(); j++) {
-				getPR(j);
-			}
-			prval = Arrays.copyOf(nowval, nowval.length);
-			System.out.println("-------------------------------------------------------");
-		}
-	}
-	double getPR(int v) {
-		double testprval = 0.0;
-		System.out.println(v + "v'svalue");
-		for(Integer eachadj : pggraph.adj(v)) {
-			System.out.println(prval[eachadj]/(pggraph.outdegree(eachadj)) + "everytime" + eachadj);
-			testprval = testprval + ((double)prval[eachadj]/(double)pggraph.outdegree(eachadj));
-		}
-		System.out.println(testprval);
-		nowval[v] = testprval;
-		return nowval[v];
-	}
-	public String toString() {
-		return Arrays.toString(prval);
-	}
-}
-
-class WebSearch {
-
-}
-
-
 public class Solution {
 	public static void main(String[] args) {
 		// read the first line of the input to get the number of vertices
 
-		int vertexnum = Integer.parseInt(StdIn.readLine());
+		int vnum = Integer.parseInt(StdIn.readLine());
 		// iterate count of vertices times
 
 		// to read the adjacency list from std input
 		// and build the graph
-		Digraph graph = new Digraph(vertexnum);
-		for (int i = 0; i < vertexnum; i++) {
+		Digraph graph = new Digraph(vnum);
+		for (int i = 0; i < vnum; i++) {
 			String[] edges = StdIn.readLine().split(" ");
 			for (int k = 1; k < edges.length; k++) {
 				graph.addEdge(Integer.parseInt(edges[0]), Integer.parseInt(edges[k]));
@@ -77,6 +33,46 @@ public class Solution {
 		// remove the q= prefix and extract the search word
 		// pass the word to iAmFeelingLucky method of web search
 		// print the return value of iAmFeelingLucky
-
 	}
+}
+class PageRank {
+	private Digraph pggraph;
+	private double[] prval;
+	private double[] nxtval;
+	PageRank(Digraph graph) {
+		this.pggraph = graph;
+		prval = new double[pggraph.V()];
+		for(int y = 0; y < prval.length; y++) {
+			prval[y] = (1.0 / (pggraph.V()));
+		}
+		nxtval = new double[pggraph.V()];
+		updatingprvals();
+	}
+	void updatingprvals() {
+		for (int i = 0; i < 1000; i++) {
+			// System.out.println("iteration number " + i);
+			for (int j = 0; j < pggraph.V(); j++) {
+				getPR(j);
+			}
+			prval = Arrays.copyOf(nxtval, nxtval.length);
+			// System.out.println("-------------------------------------------------------");
+		}
+	}
+	double getPR(int v) {
+		double testprval = 0.0;
+		// System.out.println(v + "v'svalue");
+		for(Integer eachadj : pggraph.adj(v)) {
+			// System.out.println(prval[eachadj]/(pggraph.outdegree(eachadj)) + "everytime" + eachadj);
+			testprval = testprval + ((double)prval[eachadj]/(double)pggraph.outdegree(eachadj));
+		}
+		// System.out.println(testprval);
+		nxtval[v] = testprval;
+		return nxtval[v];
+	}
+	public String toString() {
+		return Arrays.toString(prval);
+	}
+}
+class WebSearch {
+
 }
