ed1ae941b846a06b88afa997662a8d976cd4d623
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-11-04 23:31:21 +0530

    "ads2_m6_a1_1: 5/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ads2-assignments/m6/ADS-2Week-1/Solution.java
+++ ads2-assignments/m6/ADS-2Week-1/Solution.java
@@ -52,20 +52,31 @@ class PageRank {
         return str;
     }
 }
-public class Solution {
-    public static void main(String[] args) {
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused.
+    }
+    /**
+     * main fucntion.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         int vertices = Integer.parseInt(StdIn.readLine());
         Digraph graph = new Digraph(vertices);
         for (int i = 0; i < vertices; i++) {
             String[] edges = StdIn.readLine().split(" ");
             for (int k = 1; k < edges.length; k++) {
-                graph.addEdge(Integer.parseInt(edges[0]), Integer.parseInt(edges[k]));
+                graph.addEdge(Integer.parseInt(edges[0]),
+                    Integer.parseInt(edges[k]));
             }
         }
         System.out.println(graph);
         PageRank pagerank = new PageRank(graph);
         System.out.println(pagerank);
         String file = "WebContent.txt";
-
     }
 }
