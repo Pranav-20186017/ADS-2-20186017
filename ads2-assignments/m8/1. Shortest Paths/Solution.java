fac527f7fc832ec68b9a1587aa376adeccecebea
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-11-06 20:07:02 +0530

    "ads2_m8_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ads2-assignments/m8/1. Shortest Paths/Solution.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+/**
+ * class for solution.
+ */
+final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+		//unused.
+	}
+	public static void main(final String[] args) {
+		final int one = 1;
+		final int two = 2;
+		final int zero = 0;
+		Scanner sc = new Scanner(System.in);
+		ArrayList<String> station = new ArrayList<>();
+		String[] ctr = sc.nextLine().split(" ");
+		String[] stnames = sc.nextLine().split(" ");
+		for (int j = zero; j < stnames.length; j++) {
+			station.add(stnames[j]);
+		}
+		EdgeWeightedDigraph ewdg = new EdgeWeightedDigraph(Integer.parseInt(ctr[zero]));
+		for (int i = zero; i < Integer.parseInt(ctr[one]); i++) {
+			String[] data = sc.nextLine().split(" ");
+			DirectedEdge edg = new DirectedEdge(station.indexOf(data[zero]),
+			station.indexOf(data[one]), Double.parseDouble(data[two]));
+			DirectedEdge edgn = new DirectedEdge(station.indexOf(data[one]),
+			station.indexOf(data[zero]), Double.parseDouble(data[two]));
+			ewdg.addEdge(edg);
+			ewdg.addEdge(edgn);
+		}
+		int querynums = Integer.parseInt(sc.nextLine());
+		for (int k = zero; k < querynums; k++) {
+			String[] query = sc.nextLine().split(" ");
+			DijkstraSP dijkobj = new DijkstraSP(ewdg,
+			station.indexOf(query[zero]));
+			System.out.println((long)dijkobj.distTo(station.indexOf(query[one])));
+		}
+	}
+}
