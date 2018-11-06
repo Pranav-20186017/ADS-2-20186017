8373b6b91b82195074cd6f808f160262355fa0c2
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-11-06 20:16:09 +0530

    "ads2_m8_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ads2-assignments/m8/1. Shortest Paths/DirectedEdge.java
+++ ads2-assignments/m8/1. Shortest Paths/DirectedEdge.java
@@ -1,7 +1,7 @@
 /**
  * Class for directed edge.
  */
-public class DirectedEdge { 
+public class DirectedEdge {
     /**
      * vertex v.
      */
@@ -15,7 +15,7 @@ public class DirectedEdge {
      */
     private final double weight;
     /**
-     * Initializes a directed edge from vertex {@code v} to vertex {@code w} with
+     * Initializes a directed edge from vertex {@code v} to vertex {@code w}
      * the given {@code weight}.
      * @param v the tail vertex
      * @param w the head vertex
@@ -24,10 +24,10 @@ public class DirectedEdge {
      *    is a negative integer
      * @throws IllegalArgumentException if {@code weight} is {@code NaN}
      */
-    public DirectedEdge(int v, int w, double weight) {
-        this.v = v;
-        this.w = w;
-        this.weight = weight;
+    public DirectedEdge(final int source, final int destination, final double heavy) {
+        this.v = source;
+        this.w = destination;
+        this.weight = heavy;
     }
     /**
      * Returns the tail vertex of the directed edge.
