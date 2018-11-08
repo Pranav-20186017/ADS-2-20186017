993e0edb46a4b32fb76e7d0c0521ea8072922234
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-11-08 18:00:45 +0530

    "ads2_m10_a1_1: 3/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ads2-assignments/m10/Seam Carving/Solution.java
+++ ads2-assignments/m10/Seam Carving/Solution.java
@@ -5,21 +5,29 @@ import java.util.Arrays;
  */
 public class Solution {
     /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused.
+    }
+    /**
      * prints the energy matrix of the pixels.
      *
      * @param      fileName  The file name
      */
     public static void printEnergies(String fileName) {
         Picture picture = new Picture(fileName);
-        StdOut.printf("image is %d pixels wide by %d pixels high.\n", picture.width(), picture.height());
+        StdOut.printf("image is %d pixels"
+            +"wide by %d pixels high.\n", picture.width(), picture.height());
 
         SeamCarver sc = new SeamCarver(picture);
 
         StdOut.printf("Printing energy calculated for each pixel.\n");
 
         for (int row = 0; row < sc.height(); row++) {
-            for (int col = 0; col < sc.width(); col++)
+            for (int col = 0; col < sc.width(); col++) {
                 StdOut.printf("%9.0f ", sc.energy(col, row));
+            }
             StdOut.println();
         }
     }
