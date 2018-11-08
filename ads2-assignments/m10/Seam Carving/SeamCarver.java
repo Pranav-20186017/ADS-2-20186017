5f7319398e5e5f9ac66421b64d66b86263df7b0e
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-11-08 16:44:50 +0530

    "ads2_m10_a1_1: 4/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ads2-assignments/m10/Seam Carving/SeamCarver.java
+++ ads2-assignments/m10/Seam Carving/SeamCarver.java
@@ -1,61 +1,62 @@
 import java.awt.Color;
 public class SeamCarver {
+    // create a seam carver object based on the given picture
     private int width;
     private int height;
-    private Picture pic;
+    private Picture picture;
     private double[][] energyvals;
     public SeamCarver(Picture pic1) {
         width = pic1.width();
         height = pic1.height();
-        pic = pic1;
-        energyvals = new double[width][height];
+        picture = pic1;
+        energyvals = new double[height][width];
         int x = 0;
         int y = 0;
-        Color c1,c2;
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
+        Color color1,color2;
+        for (int i = 0; i< height;i++){
+            for (int j = 0; j < width; j++){
                 x = 0;
                 y = 0;
-                if (((i==0)||(j==0))||((i==(height-1))||(j==(width-1)))) {
+                if (((i==0)||(j==0))||((i==(height-1))||(j==(width-1)))){
                     energyvals[i][j] = 1000;
                 } else {
-                    c1 = pic.get(j+1, i);
-                    c2 = pic.get(j - 1, i);
-                    x += Math.pow((c1.getRed() - c2.getRed()), 2);
-                    x += Math.pow((c1.getGreen() - c2.getGreen()), 2);
-                    x += Math.pow((c1.getBlue() - c2.getBlue()), 2);
-                    c1 = pic.get(j, i +1);
-                    c2 = pic.get(j, i - 1);
-                    y += Math.pow((c1.getRed() - c2.getRed()), 2);
-                    y += Math.pow((c1.getGreen() - c2.getGreen()), 2);
-                    y += Math.pow((c1.getBlue() - c2.getBlue()), 2);
+                    color1 = picture.get(j+1, i);
+                    color2 = picture.get(j - 1, i);
+                    x += Math.pow((color1.getRed() - color2.getRed()), 2);
+                    x += Math.pow((color1.getGreen() - color2.getGreen()), 2);
+                    x += Math.pow((color1.getBlue() - color2.getBlue()), 2);
+                    color1 = picture.get(j, i +1);
+                    color2 = picture.get(j, i - 1);
+                    y += Math.pow((color1.getRed() - color2.getRed()), 2);
+                    y += Math.pow((color1.getGreen() - color2.getGreen()), 2);
+                    y += Math.pow((color1.getBlue() - color2.getBlue()), 2);
                     energyvals[i][j] = Math.sqrt(x + y);
                 }
+            }
         }
     }
-  }
-  public Picture picture() {
-    return pic;
-  }
-  public int width() {
-    return width;
-  }
-  public int height() {
-    return height;
-  }
-  public double energy(int x, int y) {
-    return energyvals[x][y];
-  }
-  public int[] findHorizontalSeam() {
-    return new int[0];
-  }
-  public int[] findVerticalSeam() {
-    return new int[0];
-  }
-  public void removeHorizontalSeam(int[] seam){
+    public Picture picture() {
+        return picture;
+    }
+    public int width() {
+        return width;
+    }
+    public int height() {
+        return height;
+    }
+    public double energy(int x, int y) {
+        return energyvals[y][x];
+    }
+    public int[] findHorizontalSeam() {
+        return new int[0];
+    }
+    public int[] findVerticalSeam() {
+        return new int[0];
+    }
+    public void removeHorizontalSeam(int[] seam) {
 
-  }
-  public void removeVerticalSeam(int[] seam) {
+    }
+    public void removeVerticalSeam(int[] seam) {
 
-  }
+    }
 }
