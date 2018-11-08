ad26dcef9f56c4ed1fdc5024e591092ca280a6f8
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-11-08 17:03:08 +0530

    "ads2_m10_a1_1: 5/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ads2-assignments/m10/Seam Carving/SeamCarver.java
+++ ads2-assignments/m10/Seam Carving/SeamCarver.java
@@ -1,10 +1,18 @@
 import java.awt.Color;
+/**
+ * Class for seam carver.
+ */
 public class SeamCarver {
     // create a seam carver object based on the given picture
     private int width;
     private int height;
     private Picture picture;
     private double[][] energyvals;
+    /**
+     * Constructs the object.
+     *
+     * @param      pic1  The picture 1
+     */
     public SeamCarver(Picture pic1) {
         if (pic1 == null) {
             System.out.println("picture is null");
@@ -39,27 +47,70 @@ public class SeamCarver {
             }
         }
     }
+    /**
+     * returns the picture object.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public Picture picture() {
         return picture;
     }
+    /**
+     * returns width of the picture.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int width() {
         return width;
     }
+    /**
+     * returns height of the picture.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int height() {
         return height;
     }
+    /**
+     * returns an array of energy values of pixels.
+     *
+     * @param      x     { parameter_description }
+     * @param      y     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
     public double energy(int x, int y) {
         return energyvals[y][x];
     }
+    /**
+     * finds the horizontal seam in a given picture.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int[] findHorizontalSeam() {
         return new int[0];
     }
+    /**
+     * finds the vertical seam in a given picture.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int[] findVerticalSeam() {
         return new int[0];
     }
+    /**
+     * Removes a horizontal seam.
+     *
+     * @param      seam  The seam
+     */
     public void removeHorizontalSeam(int[] seam) {
 
     }
+    /**
+     * Removes a vertical seam.
+     *
+     * @param      seam  The seam
+     */
     public void removeVerticalSeam(int[] seam) {
 
     }
