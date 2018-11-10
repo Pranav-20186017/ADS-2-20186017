3167a777e0c12908e4b1b829250274837dc802b2
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-11-10 10:59:12 +0530

    "ads2_m12_a1_1: 2/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ads2-assignments/m12/ADS-2_Exam-2/Solution.java
+++ ads2-assignments/m12/ADS-2_Exam-2/Solution.java
@@ -1,48 +1,48 @@
 import java.util.Scanner;
 public class Solution {
 
-	public static void main(String[] args) {
-		// Self loops are not allowed...
-		// Parallel Edges are allowed...
-		// Take the Graph input here...
-		Scanner sc = new Scanner(System.in);
-		int vertices = sc.nextInt();
-		int edges = sc.nextInt();
-		for (int i = 0; i < edges; i++) {
-			String line = sc.nextLine();
-			String[] tokens = line.split(" ");
-			int v = Integer.parseInt(tokens[0]);
-			int w = Integer.parseInt(tokens[1]);
-			int cost = Integer.parseInt(tokens[2]);
-			DirectedEdge e = new DirectedEdge(v, w, cost);
-			EdgeWeightedDigraph ed = new EdgeWeightedDigraph(vertices);
-			ed.addEdge(e);
-		}
-		String caseToGo = sc.nextLine();
-		switch (caseToGo) {
-		case "Graph":
-			EdgeWeightedDigraph d = new EdgeWeightedDigraph(vertices);
-			System.out.println(d);
-			break;
+    public static void main(String[] args) {
+        // Self loops are not allowed...
+        // Parallel Edges are allowed...
+        // Take the Graph input here...
+        Scanner sc = new Scanner(System.in);
+        int vertices = Integer.parseInt(sc.nextLine());
+        int edges = Integer.parseInt(sc.nextLine());
+        EdgeWeightedGraph ewg = new EdgeWeightedGraph(vertices);
+        int count = edges;
+        while (count > 0) {
+            String[] tokens = sc.nextLine().split(" ");
+            Edge ed = new Edge(Integer.parseInt(tokens[0]),
+                               Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]));
+            ewg.addEdge(ed);
+            count--;
+        }
 
-		case "DirectedPaths":
-			// Handle the case of DirectedPaths, where two integers are given.
-			// First is the source and second is the destination.
-			// If the path exists print the distance between them.
-			// Other wise print "No Path Found."
-			break;
+        String caseToGo = sc.nextLine();
+        switch (caseToGo) {
+        case "Graph":
+            //Print the Graph Object.
+            System.out.println(ewg);
+            break;
 
-		case "ViaPaths":
-			// Handle the case of ViaPaths, where three integers are given.
-			// First is the source and second is the via is the one where path should pass throuh.
-			// third is the destination.
-			// If the path exists print the distance between them.
-			// Other wise print "No Path Found."
-			break;
+        case "DirectedPaths":
+            // Handle the case of DirectedPaths, where two integers are given.
+            // First is the source and second is the destination.
+            // If the path exists print the distance between them.
+            // Other wise print "No Path Found."
+            break;
 
-		default:
-			break;
-		}
+        case "ViaPaths":
+            // Handle the case of ViaPaths, where three integers are given.
+            // First is the source and second is the via is the one where path should pass throuh.
+            // third is the destination.
+            // If the path exists print the distance between them.
+            // Other wise print "No Path Found."
+            break;
 
-	}
+        default:
+            break;
+        }
+
+    }
 }
