54ec70d6a09095a69d9ebb68e89e6d29879acd76
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-11-11 23:17:26 +0530

    "ads2_m10_a1_1: 6/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ads2-assignments/m10/Seam Carving/PrintEnergy.java
+++ ads2-assignments/m10/Seam Carving/PrintEnergy.java
@@ -20,8 +20,9 @@ final class PrintEnergy {
         SeamCarver sc = new SeamCarver(picture);
         StdOut.printf("Printing energy calculated for each pixel.\n");
         for (int row = 0; row < sc.height(); row++) {
-            for (int col = 0; col < sc.width(); col++)
+            for (int col = 0; col < sc.width(); col++) {
                 StdOut.printf("%9.0f ", sc.energy(col, row));
+            }
             StdOut.println();
         }
     }
