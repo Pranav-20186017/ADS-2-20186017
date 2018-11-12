6bc3edcd293dfec17ecb66c840300680bae77500
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-11-12 16:08:12 +0530

    "ads2_m13_a1_1: 7/8 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 8/10 "

--- ADS-2-Practice/temp/Radix Sorts/solution.py
+++ ADS-2-Practice/temp/Radix Sorts/solution.py
@@ -1,5 +1,9 @@
-n = int(input())
-a = []
-for i in range(n):
-	a.append(int(input()))
-print(sorted(a))
+"""
+String Sort
+
+"""
+N = int(input())
+A = []
+for i in range(N):
+    A.append(int(input()))
+print(sorted(A))
