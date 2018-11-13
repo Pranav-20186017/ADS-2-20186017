6a93a4cbecd5d769389187cd5b755e57f27975c5
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-11-13 20:46:01 +0530

    "ads2_m14_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ads2-assignments/m14/Assignment-1/Solution.java
+++ ads2-assignments/m14/Assignment-1/Solution.java
@@ -1,11 +1,41 @@
 import java.util.Scanner;
-
-public class Solution {
-	public static void main(String[] args) {
-		String[] words = loadWords();
-		//Your code goes here...
+/**
+ * Class for solution.
+ */
+final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+		//unused.
 	}
-
+	/**
+	 * main method.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
+		String[] words = loadWords();
+		Scanner sc = new Scanner(System.in);
+		TST<Integer> tst = new TST<Integer>();
+		for (int i = 0; i < words.length; i++) {
+			for (int j = 0; j < words[i].length(); j++) {
+				String str = words[i].substring(j);
+                if (!tst.contains(str)) {
+                    tst.put(str, i);
+                }
+			}
+		}
+		String tokens = sc.nextLine();
+        for (String s : tst.keysWithPrefix(tokens)) {
+            System.out.println(s);
+        }
+	}
+	/**
+	 * Loads words.
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public static String[] loadWords() {
 		In in = new In("/Files/dictionary-algs4.txt");
 		String[] words = in.readAllStrings();
