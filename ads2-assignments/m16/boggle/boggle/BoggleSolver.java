1d205ea2fcefd71689602a27cecb0a6ccf786ca9
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-11-16 18:25:58 +0530

    "ads2_m16_a1_1: 29/30 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ads2-assignments/m16/boggle/boggle/BoggleSolver.java
+++ ads2-assignments/m16/boggle/boggle/BoggleSolver.java
@@ -1,18 +1,136 @@
+import java.util.HashSet;
+/**.
+ * Class for boggle solver.
+ */
 public class BoggleSolver {
-	// Initializes the data structure using the given array of strings as the dictionary.
-	// (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
-	public BoggleSolver(String[] dictionary) {
+    /**.
+     * { var_description }
+     */
+    private TrieSET trie;
+    /**.
+     * { var_description }
+     */
+    public static final int T_H_R_E_E = 3;
+    /**.
+     * { var_description }
+     */
+    public static final int F_O_U_R = 4;
+    /**.
+     * { var_description }
+     */
+    public static final int F_I_V_E = 5;
+    /**.
+     * { var_description }
+     */
+    public static final int S_I_X = 6;
+    /**.
+     * { var_description }
+     */
+    public static final int S_E_V_E_N = 7;
+    /**.
+     * { var_description }
+     */
+    public static final int E_L_E = 11;
+    /**.
+     * Constructs the object.
+     *
+     * @param      dictionary  The dictionary
+     */
+    public BoggleSolver(final String[] dictionary) {
+        trie = new TrieSET();
+        for (int i = 0; i < dictionary.length; i++) {
+            trie.add(dictionary[i]);
+        }
+    }
+    /**.
+     * Gets all valid words.
+     *
+     * @param      board  The board
+     *
+     * @return     All valid words.
+     */
+    public HashSet<String> getAllValidWords(final BoggleBoard board) {
+        HashSet<String> words = new HashSet<String>();
 
-	}
+        for (int i = 0; i < board.rows(); i++) {
+            for (int j = 0; j < board.cols(); j++) {
+                boolean[][] visited = new boolean[
+                board.rows()][board.cols()];
+                searchWord(board, i, j, "", visited, words);
+            }
 
-	// Returns the set of all valid words in the given Boggle board, as an Iterable.
-	public Iterable<String> getAllValidWords(BoggleBoard board) {
-		return new Bag<String>();
-	}
-
-	// Returns the score of the given word if it is in the dictionary, zero otherwise.
-	// (You can assume the word contains only the uppercase letters A through Z.)
-	public int scoreOf(String word) {
-		return 0;
-	}
+        }
+        return words;
+    }
+    /**.
+     * seraches for a word on the boogle board.
+     *
+     * @param      board    The board
+     * @param      i        { parameter_description }
+     * @param      j        { parameter_description }
+     * @param      str      The string
+     * @param      visited  The visited
+     * @param      words    The words
+     */
+    private void searchWord(final BoggleBoard board, final int i,
+        final int j, final String str, final boolean[][] visited,
+                            final HashSet words) {
+        if (visited[i][j]) {
+            return;
+        }
+        char ch = board.getLetter(i, j);
+        String word = str;
+        if (ch == 'Q') {
+            word += "QU";
+        } else {
+            word += ch;
+        }
+        if (!trie.hasPrefix(word)) {
+            return;
+        }
+        if (word.length() > 2 && trie.contains(word)) {
+            words.add(word);
+        }
+        visited[i][j] = true;
+        for (int k = -1; k <= 1; k++) {
+            for (int l = -1; l <= 1; l++) {
+                if ((i + k >= 0) && (i + k < board.rows())
+                        && (j + l >= 0) && (j + l < board.cols())) {
+                    searchWord(board, i + k, j + l, word,
+                               visited, words);
+                }
+            }
+        }
+        visited[i][j] = false;
+    }
+    /**.
+     * computes the score.
+     *
+     * @param      word  The word
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int scoreOf(final String word) {
+        if (trie.contains(word)) {
+            switch (word.length()) {
+            //case 0:
+            //case 1:
+            case 2:
+                return 0;
+            case T_H_R_E_E:
+            case F_O_U_R:
+                return 1;
+            case F_I_V_E:
+                return 2;
+            case S_I_X:
+                return T_H_R_E_E;
+            case S_E_V_E_N:
+                return F_I_V_E;
+            default:
+                return E_L_E;
+            }
+        } else {
+            return 0;
+        }
+    }
 }
