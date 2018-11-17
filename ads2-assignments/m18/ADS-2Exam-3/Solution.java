a55310c46af9601effa247794425452302805fe2
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-11-17 11:26:53 +0530

    "ads2_m18_a1_1: 4/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ads2-assignments/m18/ADS-2Exam-3/Solution.java
+++ ads2-assignments/m18/ADS-2Exam-3/Solution.java
@@ -149,10 +149,44 @@ class T9 {
         // your code goes here
         return terstr.keysWithPrefix(prefix);
     }
-
     public Iterable<String> potentialWords(String t9Signature) {
         // your code goes here
-        return null;
+        // String[] nums = t9Signature.split("");
+        ArrayList<String> list = new ArrayList<>();
+        for(String itr: terstr.keys()) {
+            String[] token = itr.split("");
+            String number = "";
+            for(String ch : token){
+                if(ch.equals("a") || ch.equals("b") || ch.equals("c")) {
+                    number = number + "2";
+                }
+                if(ch.equals("d") || ch.equals("e") || ch.equals("f")) {
+                    number = number + "3";
+                }
+                if(ch.equals("g") || ch.equals("h") || ch.equals("i")) {
+                    number = number + "4";
+                }
+                if(ch.equals("j") || ch.equals("k") || ch.equals("l")) {
+                    number = number + "5";
+                }
+                if(ch.equals("m") || ch.equals("n") || ch.equals("o")) {
+                    number = number + "6";
+                }
+                if(ch.equals("p") || ch.equals("q") || ch.equals("r") || ch.equals("s")) {
+                    number = number + "7";
+                }
+                if(ch.equals("t") || ch.equals("u") || ch.equals("v")) {
+                    number = number + "8";
+                }
+                if(ch.equals("w") || ch.equals("x") || ch.equals("y") || ch.equals("z")) {
+                    number = number + "9";
+                }
+            }
+            if(number.equals(t9Signature)) {
+                list.add(itr);
+            }
+        } 
+        return list;
     }
     // final output
     // Don't modify this method.
